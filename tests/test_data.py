d08f5fa1ffc560061aab66ca8475e07e96a1886e
johannesmorkrid <johannes.morkrid@gmail.com>
2025-10-26 18:43:17 +0100

    Added "instructions" for what to implement of tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,11 @@
-import pytest
-
+import pytest 
 
 def test_code_is_tested():
     assert False
+
+"""
+    Evaluation metric requirements
+    • Should have a simple test associated with it.
+    • Keep it simple!
+    * Test that .lower works for mode
+"""
